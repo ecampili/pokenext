84eca1d89987b520acb817c23c4407ad32a6bbe1
workcampili <work.campili@gmail.com>
2022-03-19 19:10:11 -0300

    se agrego ISG y ISR

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -98,18 +98,30 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         params: {id}        
       }
     )),
-    fallback: false
+    // fallback: false
+    fallback: 'blocking'
   }
 }
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
 
   const {id} = params as {id:string} 
+  const pokemon =  await getPokemonInfo(id)
+  
+  if(!pokemon){
+    return {
+      redirect:{
+        destination:'/',
+        permanent:false
+      }
+    }
+  }
   
     return {
       props: {
-        pokemon:  await getPokemonInfo(id)
-      }
+        pokemon: pokemon
+      },
+      revalidate:86400 //degundos = dia
     }
   }
 
